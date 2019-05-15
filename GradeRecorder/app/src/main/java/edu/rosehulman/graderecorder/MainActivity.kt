6d1166a735a00cc21faf52ae170102d3a62ff727
Matt Boutell <boutell@gmail.com>
2019-05-15 11:52:32 -0400

    More tweaks to starting code

--- GradeRecorder/app/src/main/java/edu/rosehulman/graderecorder/MainActivity.kt
+++ GradeRecorder/app/src/main/java/edu/rosehulman/graderecorder/MainActivity.kt
@@ -3,10 +3,7 @@ package edu.rosehulman.graderecorder
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
 import edu.rosehulman.graderecorder.models.Assignment
 import edu.rosehulman.graderecorder.models.Course
 import edu.rosehulman.graderecorder.models.Owner
@@ -17,33 +14,30 @@ class MainActivity : AppCompatActivity() {
     private val doInitialSetup = false
 
     private val baseRef = FirebaseFirestore.getInstance()
-    private val courseRef = baseRef.collection("courses")
-    private val assignmentRef = baseRef.collection("assignments")
-    private val studentRef = baseRef.collection("students")
-    private val ownerRef = baseRef.collection("owners")
-    private val gradeEntryRef = baseRef.collection("gradeentries")
+    private val coursesRef = baseRef.collection("courses")
+    private val assignmentsRef = baseRef.collection("assignments")
+    private val studentsRef = baseRef.collection("students")
+    private val ownersRef = baseRef.collection("owners")
+    private val gradeEntriesRef = baseRef.collection("gradeentries")
 
     private val courses = ArrayList<Course>()
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                getStudentsInCourse(idFromName("CSSE483"))
+                getAssignmentsInCourse(idFromName("CSSE483"))
+                // getStudentsInCourse(idFromName("CSSE374"))
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 // I know this is kind of dumb since I know the name. But
                 // this method was called from a place where I did just have a
                 // course id.
-                setCourseNameForCourseId(idFromName("CSSE479"))
-                // getAssignmentsInCourse(idFromName("CSSE374"))
+                getCourseNameForCourseId(idFromName("CSSE479"))
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                // TODO: Write. Hint: Use FieldPath in query.
                 getCoursesForOwner("boutell")
-                // TODO: Also add owner (must be both way)
-
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -62,16 +56,18 @@ class MainActivity : AppCompatActivity() {
         // TODO: set message.text with the appropriate String.
     }
 
-    private fun getCourseNameForCourseId(courseId: String) {
+    private fun getStudentsInCourse(courseId: String) {
         // TODO: set message.text with the appropriate String.
     }
 
-    private fun getStudentsInCourse(courseId: String) {
+    private fun getCourseNameForCourseId(courseId: String) {
         // TODO: set message.text with the appropriate String.
     }
 
     fun getCoursesForOwner(ownerName: String) {
         // TODO: set message.text with the appropriate String.
+        // Hint: Use FieldPath in query.
+
     }
 
     fun getCoursesForOwner2(ownerName: String) {
@@ -98,21 +94,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addOwnerForCourse(ownerId: String, courseId: String) {
-        // TODO: add given owner to the given course.
+        // TODO: add given owner to the given course. Remember it's double-linked.
     }
 
     // Setup
     private fun pushInitialCourses() {
         if (doInitialSetup) {
-            courseRef.add(Course("CSSE483", "boutell"))
-            courseRef.add(Course("CSSE479", "chenette"))
-            courseRef.add(Course("CSSE374", "hays"))
+            coursesRef.add(Course("CSSE483", "boutell"))
+            coursesRef.add(Course("CSSE479", "chenette"))
+            coursesRef.add(Course("CSSE374", "hays"))
         }
         getCourses()
     }
 
     private fun getCourses() {
-        courseRef.get().addOnSuccessListener { snapshot ->
+        coursesRef.get().addOnSuccessListener { snapshot ->
             courses.clear()
             for (document in snapshot) {
                 courses.add(Course.fromSnapshot(document))
@@ -128,24 +124,24 @@ class MainActivity : AppCompatActivity() {
             val courseIdForCSSE374 = idFromName("CSSE374")
             val courseIdForCSSE479 = idFromName("CSSE479")
 
-            assignmentRef.add(Assignment(courseIdForCSSE483, "Lab1", 10.0))
-            assignmentRef.add(Assignment(courseIdForCSSE483, "Exam1", 45.0))
-            assignmentRef.add(Assignment(courseIdForCSSE483, "Layouts", 5.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE483, "Lab1", 10.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE483, "Exam1", 45.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE483, "Layouts", 5.0))
 
-            assignmentRef.add(Assignment(courseIdForCSSE374, "ReadingQuiz1", 10.0))
-            assignmentRef.add(Assignment(courseIdForCSSE374, "Project M1 Design", 20.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE374, "ReadingQuiz1", 10.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE374, "Project M1 Design", 20.0))
 
-            assignmentRef.add(Assignment(courseIdForCSSE479, "Vigenere", 100.0))
-            assignmentRef.add(Assignment(courseIdForCSSE479, "AES", 80.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE479, "Vigenere", 100.0))
+            assignmentsRef.add(Assignment(courseIdForCSSE479, "AES", 80.0))
 
-            studentRef.add(Student(courseIdForCSSE483, "Jianan", "Pang", "pangj"))
-            studentRef.add(Student(courseIdForCSSE483, "Alex", "Dripchak", "dripchar"))
+            studentsRef.add(Student(courseIdForCSSE483, "Jianan", "Pang", "pangj"))
+            studentsRef.add(Student(courseIdForCSSE483, "Alex", "Dripchak", "dripchar"))
 
-            studentRef.add(Student(courseIdForCSSE374, "Eugene", "Kim", "kime2"))
-            studentRef.add(Student(courseIdForCSSE374, "Yang", "Gao", "gaoy2"))
-            studentRef.add(Student(courseIdForCSSE374, "Matthew", "Lyons", "lyonsmj"))
+            studentsRef.add(Student(courseIdForCSSE374, "Eugene", "Kim", "kime2"))
+            studentsRef.add(Student(courseIdForCSSE374, "Yang", "Gao", "gaoy2"))
+            studentsRef.add(Student(courseIdForCSSE374, "Matthew", "Lyons", "lyonsmj"))
 
-            studentRef.add(Student(courseIdForCSSE479, "Jacob", "Gathof", "gathofjd"))
+            studentsRef.add(Student(courseIdForCSSE479, "Jacob", "Gathof", "gathofjd"))
         }
     }
 
@@ -154,35 +150,35 @@ class MainActivity : AppCompatActivity() {
             val boutell = Owner("boutell")
             boutell.addCourse(idFromName("CSSE483"))
             boutell.addCourse(idFromName("CSSE479"))
-            ownerRef.add(boutell)
+            ownersRef.add(boutell)
 
             // Owners and courses are 2-way, so if Boutell is an owner of 479,
             // we must record it in the owner (done above) and in the course:
             val csse479 = courseFromName("CSSE479")
             csse479.addOwner("boutell")
-            courseRef.document(idFromName("CSSE479")).set(csse479)
+            coursesRef.document(idFromName("CSSE479")).set(csse479)
 
             // Add other owners
             val chenette = Owner("chenette")
             chenette.addCourse(idFromName("CSSE479"))
-            ownerRef.add(chenette)
+            ownersRef.add(chenette)
 
             val hays = Owner("hays")
             hays.addCourse(idFromName("CSSE374"))
-            ownerRef.add(hays)
+            ownersRef.add(hays)
 
             val austinin = Owner("austinin")
             austinin.addCourse(idFromName("CSSE483"))
-            ownerRef.add(austinin)
+            ownersRef.add(austinin)
 
             val lewistd = Owner("lewistd")
             lewistd.addCourse(idFromName("CSSE483"))
-            ownerRef.add(lewistd)
+            ownersRef.add(lewistd)
 
             val csse483 = courseFromName("CSSE483")
             csse483.addOwner("austinin")
             csse483.addOwner("lewistd")
-            courseRef.document(idFromName("CSSE483")).set(csse483)
+            coursesRef.document(idFromName("CSSE483")).set(csse483)
         }
     }
 
